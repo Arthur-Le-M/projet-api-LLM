7c9e55104c18b884e3bdcab3e11dd73d58bdec8e
Arthur Le Menn <arthurlemenn29870@gmail.com>
2024-05-28 16:39:11 -0400

    feat : ajout de la pipeline complète

---
+++ webui/conv.js
@@ -0,0 +1,54 @@
+const micButton = document.getElementById('micButton');
+const audioRecords = document.getElementById('audioRecords');
+let isRecording = false;
+let audioChunks = [];
+let mediaRecorder;
+
+navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.ondataavailable = event => {
+            audioChunks.push(event.data);
+        };
+
+        mediaRecorder.onstop = async () => {
+            const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+            const formData = new FormData();
+            formData.append('file', audioBlob, 'recording.wav');
+            micButton.src = 'img/loading.svg';
+            try {
+                const response = await fetch('http://localhost:8000/conversation/', {
+                    method: 'POST',
+                    body: formData
+                });
+
+                if (response.ok) {
+                    micButton.src = 'img/micro.svg';
+                    const responseBlob = await response.blob();
+                    const responseUrl = URL.createObjectURL(responseBlob);
+                    audioRecords.src = responseUrl;
+                } else {
+                    console.error('Error from server:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+
+            audioChunks = [];
+        };
+    });
+
+micButton.addEventListener('click', () => {
+    if (!isRecording) {
+        // Start recording
+        mediaRecorder.start();
+        micButton.src = 'img/stop_mic.svg'; // Change to recording image
+        isRecording = true;
+    } else {
+        // Stop recording
+        mediaRecorder.stop();
+        micButton.src = 'img/micro.svg'; // Change back to original image
+        isRecording = false;
+    }
+});
